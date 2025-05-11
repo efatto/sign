55554585376ed11caafad59d2eaf40a4dbf23cba
kobros-tech <alkobroslymohamed@gmail.com>
2025-05-11 19:06:45 +0300

    [16.0][IMP] sign_oca: Add guided arrow flow to sign_oca

--- sign_oca/__manifest__.py
+++ sign_oca/__manifest__.py
@@ -51,6 +51,7 @@
             "sign_oca/static/src/components/sign_oca_configure/sign_oca_configure.xml",
             "sign_oca/static/src/elements/elements.xml",
             "sign_oca/static/src/scss/sign_oca.scss",
+            "sign_oca/static/src/components/sign_oca_pdf_portal/sign_oca_navigator.esm.js",
             "sign_oca/static/src/components/sign_oca_pdf_common/sign_oca_pdf_common.esm.js",
             "sign_oca/static/src/elements/text.esm.js",
             "sign_oca/static/src/elements/signature.esm.js",
